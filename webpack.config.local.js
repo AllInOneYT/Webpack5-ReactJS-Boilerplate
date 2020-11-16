33e5afa8f0a0ed49de4abb7b159d96cc5d3c1408
Edy Gunawan <gunawanedy40@gmail.com>
2020-11-16 23:15:35 +0700

    initial commit

---
+++ webpack.config.local.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const config = require("./webpack.config")
+const { merge } = require("webpack-merge")
+module.exports = merge(config, {
+    mode: "development",
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ],
+    },
+    devServer: {
+        open: true
+    }
+})
